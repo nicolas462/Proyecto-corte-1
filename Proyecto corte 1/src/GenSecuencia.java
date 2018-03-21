f3c89ad2cb6cbfedd3a58f3dbf7e737d2474ffb4
USER <USER@USER-PC>
2018-03-20 20:47:46 -0500

    Package

--- Proyecto corte 1/src/GenSecuencia.java
+++ Proyecto corte 1/src/GenSecuencia.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.Scanner;
-import Lectura.Lector;
 import java.io.*;
 
 /**
--- Proyecto corte 1/src/Lector.java
+++ Proyecto corte 1/src/Lector.java
@@ -1,4 +1,4 @@
-package Lectura;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
